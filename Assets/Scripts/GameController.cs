0e1abbe68fb6d020dff0cb7cb1f2d689f2a77978
Grigorii Boyarkin <grigorii_boyarkin@mail.ru>
2021-04-17 14:14:53 +0300

    Medium rare

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+   public GameObject settingsPanel;
+    // Start is called before the first frame update
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        settingsPanel.SetActive(true);
+    }
+    public void Play()
+    {
+        Time.timeScale = 1;
+        settingsPanel.SetActive(false);
+    }
+
+
+}
